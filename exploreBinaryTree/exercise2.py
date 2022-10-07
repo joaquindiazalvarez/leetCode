f48d63c253efac247b7a12daf20593ca533d420d
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-10-07 17:34:51 -0300

    added inorder Traversal binary tree

--- exploreBinaryTree/exercise2.py
+++ exploreBinaryTree/exercise2.py
@@ -1,16 +1,15 @@
-from typing import List, Optional
-# Definition for a binary tree node.
+from typing import Optional, List
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 class Solution:
-    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         return self.__dfs(root, [])
     def __dfs(self, root, list):
         if root == None:
             return list
-        list.append(root.val)
         list = self.__dfs(root.left, list)
+        list.append(root.val)
         return self.__dfs(root.right, list)
