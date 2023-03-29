29a126a6126dedb0b96dbbc492d4b36653df06ca
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2023-03-29 20:46:25 -0300

    added isomorphic problem

--- exploreHashmaps/Python/Ejercicio8.py
+++ exploreHashmaps/Python/Ejercicio8.py
@@ -1,14 +1,16 @@
 class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
+    def isIsomorphic(self, s: str, t: str) -> bool:    
+        return self.oneSide(s, t) and self.oneSide(t, s)
+    def oneSide(self, s:str, t:str) -> bool:
         replace_map = {}
         for i in range(len(s)):
             if s[i] in replace_map and replace_map[s[i]] != t[i]:
                 return False
             replace_map[s[i]] = t[i]
-        return True, replace_map
+        return True
     
 sol = Solution()
 print(sol.isIsomorphic("paper", "title"))
 print(sol.isIsomorphic("foo", "bar"))
 print(sol.isIsomorphic("egg", "add"))
-print(sol.isIsomorphic("baba", "badc"))
+print(sol.isIsomorphic("badc", "baba"))
