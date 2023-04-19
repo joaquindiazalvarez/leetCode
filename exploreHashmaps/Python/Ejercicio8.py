0622ec16819a125295892078ef129d2383f14a0d
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2023-04-19 11:50:13 -0400

    isomorphic incomplete

--- exploreHashmaps/Python/Ejercicio8.py
+++ exploreHashmaps/Python/Ejercicio8.py
@@ -2,7 +2,7 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         replace_map = {}
         for i in range(len(s)):
-            if s[i] in replace_map and replace_map[s[i]] != t[i]:
+            if s[i] in replace_map and replace_map[s[i]] != t[i] and s[i] != replace_map[t[i]]:
                 return False
             replace_map[s[i]] = t[i]
         return True, replace_map
@@ -11,4 +11,4 @@ sol = Solution()
 print(sol.isIsomorphic("paper", "title"))
 print(sol.isIsomorphic("foo", "bar"))
 print(sol.isIsomorphic("egg", "add"))
-print(sol.isIsomorphic("baba", "badc"))
+print(sol.isIsomorphic("badc", "baba"))
